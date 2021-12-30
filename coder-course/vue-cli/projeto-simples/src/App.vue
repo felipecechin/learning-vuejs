ccca5f5a574642de940fdf78efb50845b6e25ee3
Felipe <ficechin@hotmail.com>
2021-12-30 17:17:14 -0300

    Last exercises Vue and projects with Vue-cli.

---
+++ coder-course/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        {{ titulo }}
+        <button @click="titulo +='#'">Alterar 2</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                titulo: 'Teste data usando Vue'
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        background-color: chocolate;
+        color: #fff;
+    }
+</style>
