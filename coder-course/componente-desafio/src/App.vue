f69b4b7ea7f5db1cd54330fecd2bb322a39c4939
Felipe <ficechin@hotmail.com>
2021-12-31 12:04:55 -0300

    Components project.

---
+++ coder-course/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <app-navigation/>
+
+        <app-bar/>
+
+        <app-content/>
+        <app-footer/>
+    </v-app>
+</template>
+
+<script>
+    import Navigation from "./components/Navigation";
+    import AppBar from "./components/AppBar";
+    import Content from "./components/Content";
+    import Footer from "./components/Footer";
+
+    export default {
+        components: {
+            appNavigation: Navigation,
+            appBar: AppBar,
+            appContent: Content,
+            appFooter: Footer
+        }
+    }
+</script>
--- coder-course/componente-desafio/src/main.js
+++ coder-course/componente-desafio/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Contadores from "./Contadores";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.component('app-contadores', Contadores)
 
 new Vue({
-    render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
